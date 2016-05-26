29b7a28e3e059b8d977f6f5558460e44f77f09f9
Ryan Strenkowski <rstrenkowski@gmail.com>
2016-05-26 14:22:17 -0400

    Added the second while loop that incorporates switches to create a menu

--- WordUnscrambler.java
+++ WordUnscrambler.java
@@ -41,11 +41,59 @@ public class WordUnscrambler
 				charScramble[j] = temp;
 			}
 			
-			scrambled = String.valueOf*charScramble);
-			int counter = 0;
+			scrambled = String.valueOf(charScramble);
+			int turnCounter = 0;
+			
+			while (run && !(scrambled.equals(answer)))
+			{
+				System.out.println("------------");
+				for (int i = 0; i < scrambled.length(); i++)
+				{
+					System.out.print(i);
+				}
+				System.out.println();
+				System.out.println(scrambled + "\n------------\n");
+				System.out.println("Enter 1 to swap letters. \nEnter 2 to recieve the answer. \nEnter 3 to quit.");
+				decision = scnr.nextInt();
+				switch (decision)
+				{
+					case 1:
+					turnCounter++;
+					System.out.println("Enter the first index value. (Integer value)");
+					index1 = scnr.nextInt();
+					while(!(0 <= index1) || !(index1 < answer.length()))
+					{
+						System.out.println("Nope. Try again.");
+						index1 = scnr.nextInt();
+					}
+					System.out.println("Enter the second index value. (Integer value)");
+					index2 = scnr.nextInt();
+					while(!(0 <= index2) || !(index2 < answer.length()))
+					{
+						System.out.println("Nope. Try again.");
+						index2 = scnr.nextInt();
+					}
+					charScramble = scrambled.toCharArray();
+					temp = charScramble[index1];
+					charScramble[index1] = charScramble[index2];
+					charScramble[index2] = temp;
+					scrambled = String.valueOf(charScramble);
+					break;
+					case 2:
+					System.out.println("You chose to give up. Nobody is happy about this. The answer was: " + answer);
+					scrambled = answer;
+					end = true;
+					break;
+					case 3:
+					System.out.println("Now quitting.");
+					run = false;
+					break;
+					default:
+					System.out.println("Invalid response. 1 to swap. 2 to give up. 3 to quit.");
+					break;
+				}
+			}
 		}
-		
-		System.out.println("Congratulations! You unscrambled the word project in " + " steps.");\
 		word.close();
 	}
 }
